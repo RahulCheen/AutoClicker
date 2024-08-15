c4b4a17031e709eea371bce38ae962393462178a
Rahul Cheeniyil <69449614+RahulCheen@users.noreply.github.com>
2024-08-14 20:00:34 -0700

    Basic functionality added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,15 +3,23 @@
 #include <QTimer>
 #include <QKeyEvent>
 #include <QMessageBox>
+#include <QDesktopServices>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
     , isRunning(false) // Flag for run state of program
+    , clickTimer(new QTimer(this)) // instantiate click timer
 {
     ui->setupUi(this);
 
-    connect(ui->startButton, $QPushButton::clicked, this, &MainWindow::onStartButtonClicked);
+    connect(ui->startButton, &QPushButton::clicked, this, &MainWindow::onStartButtonClicked);
+
+    connect(clickTimer, &QTimer::timeout, this, &MainWindow::performClick);
 }
 
 MainWindow::~MainWindow()
@@ -24,16 +32,40 @@ void MainWindow::onStartButtonClicked()
     if (isRunning) {
         isRunning = false;
         ui->startButton->setText("Start");
+        clickTimer->stop();
     } else {
         QString activationKey = ui->activationEdit->text();
         int clicksPerSecond = ui->speedEdit->text().toInt();
 
         if (activationKey.isEmpty() || clicksPerSecond <= 0) {
-            QMessageBos::warning(this, "Input Error", "Please enter a valid key and click speed.");
+            QMessageBox::warning(this, "Input Error", "Please enter a valid key and click speed.");
             return;
         }
 
         isRunning = true;
         ui->startButton->setText("Stop");
+
+        int interval = 1000 / clicksPerSecond;
+        clickTimer->start(interval);
+    }
+}
+
+void MainWindow::performClick()
+{
+#ifdef _WIN32
+    INPUT input = {0};
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    SendInput(1, &input, sizeof(INPUT));
+
+    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(1, &input, sizeof(INPUT));
+#endif
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    if (event->text().toUpper() == activationKey.toUpper()) {
+        onStartButtonClicked(); // Toggle autoclicker when activation key is pressed
     }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QKeyEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,11 +19,19 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void keyPressEvent(QKeyEvent *event) override; // to handle activation key
+
 private slots:
     void onStartButtonClicked(); // Slot to handle start/stop button clicks
+    void performClick(); // Slot to do clicking
 
 private:
     Ui::MainWindow *ui;
     bool isRunning; // Flag for run state of program
+    QTimer *clickTimer; // Timer to handle click interval
+    QString activationKey; // Stores activation key
+    int clicksPerSecond; // Stores click speed
 };
+
 #endif // MAINWINDOW_H
